4838d3711e596fb1450330e466c9be582638876b
User21 <user21@user21de-imac.local>
2019-01-12 16:50:25 +0800

    Initial Commit

---
+++ friend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  friend
+//
+//  Created by User21 on 2019/1/12.
+//  Copyright © 2019 User21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
